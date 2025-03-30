22a9e32fd618473fff6d1ef7746ddb4082a45b14
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2025-03-31 01:34:18 +0200

    Edit in register file

--- RegisterFile/RegisterFile.vhd
+++ RegisterFile/RegisterFile.vhd
@@ -1,61 +1,60 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
-
 use IEEE.NUMERIC_STD.ALL;
 
 
-entity RegisterFile is
-    Port (
-        clk       : in STD_LOGIC;
-        regWrite  : in STD_LOGIC;
-        readReg1  : in STD_LOGIC_VECTOR (4 downto 0);
-        readReg2  : in STD_LOGIC_VECTOR (4 downto 0);
-        writeReg  : in STD_LOGIC_VECTOR (4 downto 0);
-        writeData : in STD_LOGIC_VECTOR (31 downto 0);
-        readData1 : out STD_LOGIC_VECTOR (31 downto 0);
-        readData2 : out STD_LOGIC_VECTOR (31 downto 0)
+ENTITY registerFile IS
+    PORT (
+        clk        : IN STD_LOGIC;
+        reg_write  : IN STD_LOGIC;
+        read_reg1  : IN STD_LOGIC_VECTOR (4 DOWNTO 0);
+        read_reg2  : IN STD_LOGIC_VECTOR (4 DOWNTO 0);
+        write_reg  : IN STD_LOGIC_VECTOR (4 DOWNTO 0);
+        write_data : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+        read_data1 : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+        read_data2 : OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
     );
-end RegisterFile;
+END registerFile;
 
-architecture Behavioral of RegisterFile is
-    type reg_array is array (0 to 31) of STD_LOGIC_VECTOR (31 downto 0);
-    signal registers : reg_array := (others => (others => '0'));
-    signal clk_delayed : STD_LOGIC := '0';
+ARCHITECTURE registerFileBehavior OF registerFile IS
+    -- Register file declaration and intialization
+    TYPE reg_array IS ARRAY (0 TO 31) OF STD_LOGIC_VECTOR (31 DOWNTO 0);
+    SIGNAL registers : reg_array := (OTHERS => (OTHERS => 'U'));
     
-    -- Register 0 is hardwired to zero in MIPS architecture
-    constant ZERO_REG : integer := 0;
+    -- Register 0 is always ZERO
+    CONSTANT ZERO_REG : integer := 0;
 
-begin
-    process (clk)
-    begin
-        if rising_edge(clk) then
-            clk_delayed <= not clk_delayed;
-            -- Writing in the first half cycle
-            if clk_delayed = '0' then
-		if RegWrite = '1' then
-                	-- Don't write to register 0 (always zero)
-                	if to_integer(unsigned(writeReg)) /= ZERO_REG then
-                    		registers(to_integer(unsigned(writeReg))) <= writeData;
-                	end if;
-            	end if;
-                
-	    -- Reading in the second half cycle
+    -- Copy write data
+    SIGNAL write_data_temp: STD_LOGIC_VECTOR (31 DOWNTO 0);
+
+    BEGIN
+    reg_file_process1: process (clk)
+    BEGIN
+	-- Writing in the first half cycle
+        if rising_edge(clk) THEN
+	    if reg_write = '1' THEN
+            -- Don't write to register 0 (always zero)
+                if to_integer(unsigned(write_reg)) /= ZERO_REG THEN
+		    write_data_temp <= write_data;
+                END IF;
+            END IF;	
+	END IF;
+	-- Reading in the second half cycle
+	if falling_edge(clk) THEN
+	    if (reg_write = '1' and write_reg = read_reg1) then
+                read_data1 <= write_data_temp;
             else
-            -- Read port 1
-            	if to_integer(unsigned(readReg1)) = ZERO_REG then
-                	readData1 <= (others => '0');
-            	else
-                	readData1 <= registers(to_integer(unsigned(readReg1)));
-            	end if;
-            
-            -- Read port 2
-            	if to_integer(unsigned(readReg2)) = ZERO_REG then
-                	readData2 <= (others => '0');
-            	else
-                	readData2 <= registers(to_integer(unsigned(readReg2)));
-            	end if;
+                read_data1 <= registers(to_integer(unsigned(read_reg1)));
             end if;
-        end if;
-    end process;
-end Behavioral;
+	    if (reg_write = '1' and write_reg = read_reg2) then
+                read_data2 <= write_data_temp;
+            else
+                read_data2 <= registers(to_integer(unsigned(read_reg2)));
+            end if;
+	END IF;
+    END PROCESS;
+	
+	-- Update register
+	registers(to_integer(unsigned(write_reg))) <= write_data_temp when (reg_write = '1');
+
+end registerFileBehavior;
